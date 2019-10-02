2fe6e16dccdf58db0e9f0c11c9b7f8dc500e881e
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-10-02 14:06:03 -0400

    Adding items to an order with

---
+++ LetsDine/SwiftUIViews/OrderView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    var body: some View {
+        NavigationView{
+            List{
+                Section{
+                    ForEach(order.items){ item in
+                        HStack{
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                Section{
+                    NavigationLink(destination: Text("Check Out")){
+                        Text("Place Order")
+                    }
+                }
+            }
+            .navigationBarTitle("Order")
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView().environmentObject(order)
+    }
+}
