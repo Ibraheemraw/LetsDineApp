79cc9f848263c9aba85f2189a3dd65511d6097ef
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-10-02 12:16:24 -0400

    added the images for the types of restrictions for the items in the list

--- LetsDine/SwiftUIViews/ItemRow.swift
+++ LetsDine/SwiftUIViews/ItemRow.swift
@@ -1,13 +1,29 @@
 import SwiftUI
 
 struct ItemRow: View {
+    static let colors: [String: Color] = ["D": .purple, "G": .black, "N":.red, "S":.blue, "V": .green]
     var item: MenuItem
     var body: some View {
         HStack{
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.blue, lineWidth: 2))
             VStack(alignment: .leading){
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+            }.layoutPriority(1)
+
+            Spacer()
+
+            ForEach(item.restrictions, id: \.self){restrictions in
+                Text(restrictions)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(Self.colors[restrictions, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
